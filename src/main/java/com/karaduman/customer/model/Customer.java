c088aab192bd6a17fc4b9291479e7bcb5419e0e4
olckara <olckara@gmail.com>
2024-06-08 09:07:34 +0200

    add customer id

--- src/main/java/com/karaduman/customer/model/Customer.java
+++ src/main/java/com/karaduman/customer/model/Customer.java
@@ -3,12 +3,16 @@ package com.karaduman.customer.model;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.AccessType;
+import org.springframework.data.annotation.Id;
 
 import java.util.Date;
 
 @Data
+@AccessType(AccessType.Type.PROPERTY)
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class Customer {
+    private @Id String customerId;
     private String name;
     private Address address;
     private Date dateOfBirth;
