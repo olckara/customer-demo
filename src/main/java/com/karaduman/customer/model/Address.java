e2a753a4f48aae5f68fefaf0c11bf47efe908d42
olckara <olckara@gmail.com>
2024-06-08 09:07:12 +0200

    add Lombok

--- src/main/java/com/karaduman/customer/model/Address.java
+++ src/main/java/com/karaduman/customer/model/Address.java
@@ -1,47 +1,15 @@
 package com.karaduman.customer.model;
 
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class Address {
     private String postCode;
     private String houseNumber;
     private String houseNumberAddition;
     private String city;
-
-    public Address(String postCode, String houseNumber, String houseNumberAddition, String city) {
-        this.postCode = postCode;
-        this.houseNumber = houseNumber;
-        this.houseNumberAddition = houseNumberAddition;
-        this.city = city;
-    }
-
-    public String getPostCode() {
-        return postCode;
-    }
-
-    public void setPostCode(String postCode) {
-        this.postCode = postCode;
-    }
-
-    public String getHouseNumber() {
-        return houseNumber;
-    }
-
-    public void setHouseNumber(String houseNumber) {
-        this.houseNumber = houseNumber;
-    }
-
-    public String getHouseNumberAddition() {
-        return houseNumberAddition;
-    }
-
-    public void setHouseNumberAddition(String houseNumberAddition) {
-        this.houseNumberAddition = houseNumberAddition;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
 }
--- src/main/java/com/karaduman/customer/model/Customer.java
+++ src/main/java/com/karaduman/customer/model/Customer.java
@@ -1,59 +1,17 @@
 package com.karaduman.customer.model;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class Customer {
     private String name;
     private Address address;
     private Date dateOfBirth;
     private String idDocument;
     private String username;
-
-    public Customer(String name, Address address, Date dateOfBirth, String idDocument, String username) {
-        this.name = name;
-        this.address = address;
-        this.dateOfBirth = dateOfBirth;
-        this.idDocument = idDocument;
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getIdDocument() {
-        return idDocument;
-    }
-
-    public void setIdDocument(String idDocument) {
-        this.idDocument = idDocument;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
